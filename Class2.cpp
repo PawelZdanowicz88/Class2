508fcadf5d9aac6fdf3c3d6c8825ad7dd35a5262
PawelZdanowicz88 <zdanowiczpawel88@gmail.com>
2022-09-24 16:06:25 +0200

    completed the second task

--- Class2.cpp
+++ Class2.cpp
@@ -1,6 +1,7 @@
 ﻿
 #include <iostream>
 #include <string>
+
 class Counter {
 private:
     int value;
@@ -9,13 +10,17 @@ public:
         value(v1)
     {
     }
-    void add() {
+    Counter() :
+        value(1)
+    {
+    }
+    void increase() {
         value = value + 1;
     }
-    void diff() {
+    void decrease() {
         value = value - 1;
     }
-    int equality() {
+    int present() {
         return value;
     }
 };
@@ -24,34 +29,38 @@ int main(int argc, char** argv)
 {
     setlocale(LC_ALL, "Russian");
     system("chcp 1251");
-    int v1 = 1;
+    int v1 = 0;
     std::string answer;
+    
     do {
         std::cout << "Вы хотите указать начальное значение счётчика? Введите 'да' или 'нет': ";
         std::cin >> answer;
         if (answer == "да") {
             std::cout << "Введите начальное значение счётчика: ";
             std::cin >> v1;
+            
             break;
         }
         if (answer == "нет") {
             std::cout << "Начальное значение счетчика задается по умолчанию" << std::endl;
+            
             break;
         }
         std::cout << "Ошибка в ответе, попробуйте еще раз" << std::endl;
     } while (1);
-    Counter electronics1(v1);
+    
+    Counter electronics = ((v1 != 0)?(Counter (v1)):(Counter ()));
     do {
         std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
         std::cin >> answer;
         if (answer == "+") {
-            electronics1.add();
+            electronics.increase();
         }
         else if (answer == "-") {
-            electronics1.diff();
+            electronics.decrease();
         }
         else if (answer == "=") {
-            std::cout << electronics1.equality() << std::endl;
+            std::cout << electronics.present() << std::endl;
         } 
         else if (answer == "х") {
             std::cout << "До свидания!" << std::endl;
